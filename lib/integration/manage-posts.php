27038e61373e6afb11bd7e048c01fb923ed0dd85
Duncan Brown <duncanjbrown@gmail.com>
2014-05-06 18:50:39 +0100

    death to $_GET

--- lib/integration/manage-posts.php
+++ lib/integration/manage-posts.php
@@ -4,16 +4,21 @@
  */
 add_action( 'restrict_manage_posts', 'acs_add_taxonomy_filters' );
 
+/**
+ * Add a taxonomy filter dropdown to the ACS summary page
+ * @return void 
+ */
 function acs_add_taxonomy_filters() {
 	global $typenow;
  
-	// must set this to the post type you want the filter(s) displayed on
 	if( ACS_POST_TYPE === $typenow ){
  	
  		$tax_slug = ACS_TAX;
 		$taxonomy = get_taxonomy( ACS_TAX );
 		$tax_name = $taxonomy->labels->name;
 		$terms = get_terms( ACS_TAX );
+
+		$selected = isset( $_REQUEST[ACS_TAX] ) ? $_REQUEST[ACS_TAX] : null;
 
 		if( $terms ) {
 			include( 'views/taxonomy-dropdown.php' );
